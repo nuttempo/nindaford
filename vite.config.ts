fa71207a5aed77ea0b5a793c6ad015c3cca0983d
nuttempo <nuttempo@gmail.com>
2026-01-01 12:02:05 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/nindaford/", // <-- ใส่ชื่อ repo ของคุณตรงนี้
+  base: "/nindaford/",        // สำคัญมาก
+  build: {
+    outDir: "docs",          // ให้ build ไปลง docs
+  },
 });
